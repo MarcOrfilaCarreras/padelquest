97dcd54412daff99a3360cf755a161464e3cb9c4
Marc Orfila Carreras <marcorfila2003@gmail.com>
2023-07-30 20:46:44 +0200

    fix: Enabling the plugin system to work within Docker

--- app/entrypoint.py
+++ app/entrypoint.py
@@ -1,5 +1,18 @@
 # -*- coding: utf-8 -*-
+import importlib
+import os
+
 from app import app
 
 if __name__ == "__main__":
+    if os.path.isdir("plugins"):
+        for plugin_folder in os.listdir('plugins'):
+            if os.path.isdir(os.path.join('plugins', plugin_folder)):
+                for plugin_file in os.listdir(os.path.join('plugins', plugin_folder)):
+                    if plugin_file == 'entrypoint.py':
+                        # Remove the '.py' extension from plugin_file
+                        plugin_name = plugin_file[:-3]
+                        plugin_module = importlib.import_module(
+                            f'plugins.{plugin_folder}.{plugin_name}')
+                        plugin_module.register_plugin(app)
     app.run()
