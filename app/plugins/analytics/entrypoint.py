fd559b6e907414581b250fd9821fb1ada8c95ca6
Marc Orfila Carreras <marcorfila2003@gmail.com>
2023-08-05 21:25:18 +0200

    feat: Added a new route to get analytics by date

--- app/plugins/analytics/entrypoint.py
+++ app/plugins/analytics/entrypoint.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import datetime
 import os
+import re
+import threading
 
 from flask import abort
 from flask import Blueprint
@@ -12,16 +14,23 @@ from .config import ENABLED
 from .config import ENABLED_API
 from .models.models import Day
 from .models.models import URL
+from .models.schemas import DaySchema
 from .models.schemas import URLSchema
 
-# Create the engine and session
+# Create the engine and session for database connection
 engine = create_engine(
     f'sqlite:///{os.path.dirname(os.path.abspath(__file__))}/analytics.sqlite')
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Create the blueprint
+# Create the blueprint for the plugin
 plugin_blueprint = Blueprint('plugin', __name__)
+
+
+def is_valid_date(date_str):
+    # Define a regular expression to validate the date format 'YYYY-MM-DD'
+    date_pattern = r'^\d{4}-\d{2}-\d{2}$'
+    return re.match(date_pattern, date_str)
 
 
 def create_analytics(routes):
@@ -48,18 +57,14 @@ def create_analytics(routes):
     session.commit()
 
 
-@plugin_blueprint.after_app_request
-def save_analytics(response):
+def save_analytics_thread(url):
     """
-    Save analytics data after each request if analytics is enabled.
+    Save analytics data in a separate thread.
 
-    :param response: The response object from the request.
-    :return: The response object.
+    :param url: The URL to update analytics for.
     """
     if ENABLED:
         try:
-            url = request.url_rule.rule
-
             # Retrieve the URL entry from the database
             url_entry = session.query(URL).get(url)
             if url_entry:
@@ -85,8 +90,22 @@ def save_analytics(response):
 
                 # Commit the changes to the database
                 session.commit()
-        except:
+        except Exception as e:
             pass
+
+
+@plugin_blueprint.after_app_request
+def save_analytics(response):
+    """
+    Save analytics data after each request if analytics is enabled.
+
+    :param response: The response object from the request.
+    :return: The response object.
+    """
+
+    thread = threading.Thread(
+        target=save_analytics_thread, args=(request.url_rule.rule,))
+    thread.start()
 
     return response
 
@@ -106,7 +125,7 @@ def get_analytics():
         if len(urls) == 0:
             response = {"status": "fail", "data": {
                 "message": "Analytics were not found"}}, 206
-
+            return response
         else:
             # Serialize the URL objects using the URLSchema and return the data as JSON
             return {"status": "success", "data": url_schema.dump(urls)}
@@ -115,20 +134,58 @@ def get_analytics():
         return abort(404)
 
 
+@plugin_blueprint.route('/plugins/analytics/<string:day>')
+def get_analytics_by_day(day):
+    """
+    Get the analytics data from the database and return it as a JSON response for a specific day.
+
+    :param day: The day in 'YYYY-MM-DD' format for which to retrieve analytics.
+    :return: JSON response containing the analytics data.
+    """
+    if ENABLED_API:
+        try:
+            # Validate the day format before parsing it as a datetime.date object
+            if not is_valid_date(day):
+                return {"status": "fail", "data": {"message": "Invalid date format. Please use the format YYYY-MM-DD"}}, 400
+
+            # Convert the day from the URL segment to a datetime.date object
+            day_date = datetime.datetime.strptime(day, '%Y-%m-%d').date()
+
+            day_schema = DaySchema(many=True)
+
+            day_data = session.query(Day).filter(Day.date == day_date).all()
+
+            if len(day_data) == 0:
+                response = {"status": "fail", "data": {
+                    "message": "Analytics were not found"}}, 206
+                return response
+            else:
+                # Serialize the URL objects using the URLSchema and return the data as JSON
+                return {"status": "success", "data": day_schema.dump(day_data)}
+        except Exception as e:
+            # Log the exception for debugging purposes
+            return {"status": "fail", "data": {"message": "An error occurred while processing the request"}}, 500
+    else:
+        # If the analytics API is not enabled, return a 404 error
+        abort(404)
+
+
 def register_plugin(app):
     """
     Register the plugin's blueprint with the Flask app and create/update analytics data.
 
     :param app: The Flask app instance.
     """
-
+    # Create or update analytics data for the routes in the app
     if ENABLED:
         routes = [rule.rule for rule in app.url_map.iter_rules()
                   if 'static' not in rule.endpoint]
         create_analytics(routes)
 
+    # Register the blueprint with the app
     app.register_blueprint(plugin_blueprint)
 
+    # Create or update analytics data again after registration
     if ENABLED:
         routes = [rule.rule for rule in app.url_map.iter_rules()
                   if 'static' not in rule.endpoint]
--- app/plugins/analytics/models/schemas.py
+++ app/plugins/analytics/models/schemas.py
@@ -2,6 +2,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow import fields
 
+from .models import Day
 from .models import ma
 from .models import URL
 
@@ -14,3 +15,11 @@ class URLSchema(ma.SQLAlchemySchema):
 
     name = ma.auto_field()
     total_visits = ma.auto_field()
+
+
+class DaySchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Day
+
+    total_visits = ma.auto_field()
+    url = ma.auto_field()
