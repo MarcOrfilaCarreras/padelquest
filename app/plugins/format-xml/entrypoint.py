657e090464aacf8048b455dfdb394c136f24e3c2
Marc Orfila Carreras <marcorfila2003@gmail.com>
2023-09-10 20:11:58 +0200

    feat: Added support to return XML responses

---
+++ app/plugins/format-xml/entrypoint.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import json
+
+from flask import Blueprint
+from flask import request
+from flask import Response
+from json2xml import json2xml
+
+from .config import ENABLED
+
+# Create the blueprint for the plugin
+plugin_blueprint = Blueprint('format-xml', __name__)
+
+
+@plugin_blueprint.after_app_request
+def return_xml(response):
+    """
+    Convert the response content to xml format.
+
+    :param response: The response object from the request.
+    :return: The response object.
+    """
+
+    if request.args.get("format") and (request.args.get("format") == "xml"):
+        if response.is_json:
+            data = json.loads(response.get_data(as_text=True))
+
+            xml_string = json2xml.Json2xml(
+                data, wrapper="root", pretty=True, attr_type=False).to_xml()
+
+            # Create a new Response object with the XML content
+            xml_response = Response(xml_string, content_type='application/xml')
+
+            return xml_response
+    return response
+
+
+def register_plugin(app):
+    """
+    Register the plugin's blueprint with the Flask app.
+
+    :param app: The Flask app instance.
+    """
+
+    # Register the blueprint with the app
+    if ENABLED:
+        app.register_blueprint(plugin_blueprint)
